debafa8fce278ae9ccf76f36321cf8b36b2ac271
Confidence Madiope <confidencem97@gmail.com>
2022-09-28 11:20:27 +0200

    ADD: crew-specialist and dot navigation

---
+++ starter-code/main.js
@@ -0,0 +1,11 @@
+const crewNav = document.querySelector('.crew-nav') 
+
+const dots = document.getElementsByClassName('dot')
+
+for (let i =0; i < dots.length; i ++) {
+  dots[i].addEventListener('click', function() {
+    let current = document.getElementsByName('active');
+    current[0].className = current[0].className.replace('active', '');
+    this.className += 'active'
+  })
+}
